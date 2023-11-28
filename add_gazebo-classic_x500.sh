389d79147f14451317388f241f07efef81c972c3
Romain <romain.salvagni@etu.umontpellier.fr>
2023-11-28 17:17:07 +0100

    path modif

--- add_gazebo-classic_x500.sh
+++ add_gazebo-classic_x500.sh
@@ -3,19 +3,19 @@
 #./add_gazebo-classic_x500.sh /path/to/PX4_Autopilot
 
 # Add x500 folder in Tools/simulation/gazebo-classic/sitl_gazebo-classic/models/
-cp -r $1/x500_modif/x500 "$1/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic/models/"
+cp -r /x500 "$1/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic/models/"
 
 # Add x500.world in Tools/simulation/gazebo-classic/sitl_gazebo-classic/worlds/
-cp $1/x500_modif/x500.world "$1/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic/worlds/"
+cp x500.world "$1/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic/worlds/"
 
 # Add 1063_gazeo-classic_x500 in ROMFS/px4fmu_common/init.d-posix/airframes/
-cp $1/x500_modif/1063_gazebo-classic_x500 "$1/PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/airframes/"
+cp 1063_gazebo-classic_x500 "$1/PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/airframes/"
 
 # Replace CMakeLists.txt in ROMFS/px4fmu_common/init.d-posix/airframes/
-cp -f $1/x500_modif/CMakeLists.txt "$1/PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/airframes/"
+cp -f CMakeLists.txt "$1/PX4-Autopilot/ROMFS/px4fmu_common/init.d-posix/airframes/"
 
 # Replace sitl_targets_gazebo-classic.cmake in src/modules/simulation/simulator_mavlink/
-cp -f $1/x500_modif/sitl_targets_gazebo-classic.cmake "$1/PX4-Autopilot/src/modules/simulation/simulator_mavlink/"
+cp -f sitl_targets_gazebo-classic.cmake "$1/PX4-Autopilot/src/modules/simulation/simulator_mavlink/"
 
 cd /$1/PX4-Autopilot
 
